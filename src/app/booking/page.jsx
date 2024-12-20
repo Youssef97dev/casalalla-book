6e3ddaffb0ac6fb42a113577031104af16a66d84
Youssef97dev <youssefmoussafi97@gmail.com>
2024-12-20 16:45:43 +0100

    add new reservation

---
+++ src/app/booking/page.jsx
@@ -0,0 +1,11 @@
+import Book from "./components/Book";
+
+const page = () => {
+  return (
+    <div className="bg-[#F5EDE2] w-full h-[120vh] px-0 lg:px-20 xl:px-96 py-0 lg:py-10">
+      <Book />
+    </div>
+  );
+};
+
+export default page;
