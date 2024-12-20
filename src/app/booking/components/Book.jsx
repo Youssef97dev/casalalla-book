0b9d28eb3170cc7afe0683d34ee17fde721b8c29
Youssef97dev <youssefmoussafi97@gmail.com>
2024-12-20 16:51:20 +0100

    add new reservation 2

--- src/app/booking/components/Book.jsx
+++ src/app/booking/components/Book.jsx
@@ -366,7 +366,7 @@ const Book = () => {
                 <h1 className="text-[24px] leading-[26.4px] mr-auto text-gray-800">{`Please verify your information`}</h1>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">Date:</span>
-                  <strong>
+                  <strong className="text-sm">
                     {new Intl.DateTimeFormat("en-En", {
                       day: "numeric",
                       month: "long",
@@ -376,27 +376,27 @@ const Book = () => {
                 </div>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">Time:</span>
-                  <strong>{timeBook}</strong>
+                  <strong className="text-sm">{timeBook}</strong>
                 </div>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">People:</span>
-                  <strong>{numberGuests}</strong>
+                  <strong className="text-sm">{numberGuests}</strong>
                 </div>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">First name:</span>
-                  <strong>{formData.firstName}</strong>
+                  <strong className="text-sm">{formData.firstName}</strong>
                 </div>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">Last name:</span>
-                  <strong>{formData.lastName}</strong>
+                  <strong className="text-sm">{formData.lastName}</strong>
                 </div>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">Phone:</span>
-                  <strong>{formData.phone}</strong>
+                  <strong className="text-sm">{formData.phone}</strong>
                 </div>
                 <div className="w-full flex justify-start items-center gap-5 border-t border-gray-400 py-3 px-2">
                   <span className="w-[30%]">Email:</span>
-                  <strong>{formData.email}</strong>
+                  <strong className="text-sm">{formData.email}</strong>
                 </div>
               </div>
             )}
